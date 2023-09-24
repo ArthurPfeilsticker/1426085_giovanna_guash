d9dee7f5edfbb5de2391b1d1e30ccfeaaad924f8
ArthurPfeilsticker <arthurlfpfeil@gmail.com>
2023-09-24 14:10:01 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,122 @@
+// Dados mockados para os álbuns e personagens
+const albums = [
+    {
+        title: "Fishmen Island",
+        image: "./assets/images/fishmen-island.webp",
+        description: "A tripulação do Chapéu de Palha chega a uma ilha subaquática, enfrentando tensões raciais entre humanos e tritões, enquanto protege a ilha de Hody Jones e sua tripulação.",
+        latitude: "35.6895",
+        longitude: "139.6917",
+        startDate: "2010",
+        characters: [
+            {
+                name: "Hody Jones",
+                image: "./assets/images/antagonists/hody-jones.jpeg"
+            },
+            {
+                name: "Vander Decken IX",
+                image: "./assets/images/antagonists/vander-decken-ix.webp"
+            },
+            // ... outros personagens
+        ]
+    },
+    {
+        title: "Punk Hazard",
+        image: "./assets/images/punk-hazard.jpg",
+        description: "Em uma ilha misteriosamente metade congelada e metade em chamas, Luffy e sua tripulação se envolvem em experimentos perigosos e enfrentam o ex-almirante da Marinha, Aokiji, e o cientista louco, Caesar Clown.",
+        latitude: "35.6580",
+        longitude: "139.7016",
+        startDate: "2012",
+        characters: [
+            {
+                name: "Cesar Clown",
+                image: "./assets/images/antagonists/cesar-clown.jpg"
+            },
+            {
+                name: "Monet",
+                image: "./assets/images/antagonists/monet.webp"
+            },
+            {
+                name: "Vergo",
+                image: "./assets/images/antagonists/vergo.webp"
+            },
+            // ... outros personagens
+        ]
+    },
+    {
+        title: "Dressrosa",
+        image: "./assets/images/dressrosa.jpg",
+        description: "Em uma ilha governada pelo cruel Doflamingo, a tripulação do Chapéu de Palha se alia a antigos inimigos e participa de um torneio mortal para obter a poderosa fruta Mera Mera no Mi.",
+        latitude: "35.7134",
+        longitude: "139.7766",
+        startDate: "2014",
+        characters: [
+            {
+                name: "Doflamingo",
+                image: "./assets/images/antagonists/doflamingo.jpg"
+            },
+            // ... outros personagens
+        ]
+    },
+    {
+        title: "Zou",
+        image: "./assets/images/zou.webp",
+        description: "Localizado nas costas de um elefante gigante, a tripulação descobre a história dos Minks e a conexão de Sanji com a família Vinsmoke, enquanto aprendem sobre os Poneglyphs e a busca por Laughtale.",
+        latitude: "35.7148",
+        longitude: "139.7967",
+        startDate: "2016",
+        characters: [
+            {
+                name: "Jack",
+                image: "./assets/images/antagonists/jack.webp"
+            },
+            // ... outros personagens
+        ]
+    },
+    {
+        title: "Whole Cake",
+        image: "./assets/images/whole-cake.jpeg",
+        description: "Luffy e parte da tripulação vão resgatar Sanji de seu casamento arranjado com a filha da Yonkou Big Mom, enfrentando desafios em uma ilha feita inteiramente de doces.",
+        latitude: "35.6285",
+        longitude: "139.7791",
+        startDate: "2017",
+        characters: [
+            {
+                name: "Big Mom",
+                image: "./assets/images/antagonists/big-mom.webp"
+            },
+            {
+                name: "Charlotte Katakuri",
+                image: "./assets/images/antagonists/katakuri.webp"
+            },
+            // ... outros personagens
+        ]
+    },
+    {
+        title: "Wano",
+        image: "./assets/images/wano.avif",
+        description: "Em uma nação isolada com uma cultura samurai, a tripulação do Chapéu de Palha se alia a antigos amigos e guerreiros locais para derrubar o shogun Orochi e o Yonkou Kaido.",
+        latitude: "35.7021",
+        longitude: "139.7753",
+        startDate: "2021",
+        characters: [
+            {
+                name: "Kaidou",
+                image: "./assets/images/antagonists/kaidou.webp"
+            },
+            {
+                name: "King",
+                image: "./assets/images/antagonists/king.jpg"
+            },
+            {
+                name: "Queen",
+                image: "./assets/images/antagonists/queen.png"
+            },
+            {
+                name: "Jack",
+                image: "./assets/images/antagonists/jack.webp"
+            },
+            // ... outros personagens
+        ]
+    },
+    // ... outros álbuns
+];
